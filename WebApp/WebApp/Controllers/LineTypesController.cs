2b221f1f6fac2d12d493288a50e78c8e97dba7b4
PC <sretennenadovic@gmail.com>
2019-05-29 15:00:14 +0200

    Added two of controllers.

---
+++ WebApp/WebApp/Controllers/LineTypesController.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using WebApp.Models;
+using WebApp.Persistence;
+using WebApp.Persistence.UnitOfWork;
+
+namespace WebApp.Controllers
+{
+    public class LineTypesController : ApiController
+    {
+        private IUnitOfWork db;
+
+        public LineTypesController(IUnitOfWork db)
+        {
+            this.db = db;
+        }
+
+        // GET: api/LineTypes
+        public IEnumerable<LineType> GetLineTypes()
+        {
+            return db.LineTypes.GetAll();
+        }
+
+        // GET: api/LineTypes/5
+        [ResponseType(typeof(LineType))]
+        public IHttpActionResult GetLineType(int id)
+        {
+            LineType lineType = db.LineTypes.Get(id);
+            if (lineType == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(lineType);
+        }
+
+        // PUT: api/LineTypes/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutLineType(int id, LineType lineType)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != lineType.Id)
+            {
+                return BadRequest();
+            }
+
+            db.LineTypes.Update(lineType);
+
+            try
+            {
+                db.Complete();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!LineTypeExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/LineTypes
+        [ResponseType(typeof(LineType))]
+        public IHttpActionResult PostLineType(LineType lineType)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.LineTypes.Add(lineType);
+            db.Complete();
+
+            return CreatedAtRoute("DefaultApi", new { id = lineType.Id }, lineType);
+        }
+
+        // DELETE: api/LineTypes/5
+        [ResponseType(typeof(LineType))]
+        public IHttpActionResult DeleteLineType(int id)
+        {
+            LineType lineType = db.LineTypes.Get(id);
+            if (lineType == null)
+            {
+                return NotFound();
+            }
+
+            db.LineTypes.Remove(lineType);
+            db.Complete();
+
+            return Ok(lineType);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool LineTypeExists(int id)
+        {
+            return db.LineTypes.GetAll().Count(e => e.Id == id) > 0;
+        }
+    }
+}
---
+++ WebApp/WebApp/Controllers/LinesController.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using WebApp.Models;
+using WebApp.Persistence;
+using WebApp.Persistence.UnitOfWork;
+
+namespace WebApp.Controllers
+{
+    public class LinesController : ApiController
+    {
+        private IUnitOfWork db;
+
+        public LinesController(IUnitOfWork db)
+        {
+            this.db = db;
+        }
+
+        // GET: api/Lines
+        public IEnumerable<Line> GetLines()
+        {
+            return db.Lines.GetAll();
+        }
+
+        // GET: api/Lines/5
+        [ResponseType(typeof(Line))]
+        public IHttpActionResult GetLine(int id)
+        {
+            Line line = db.Lines.Get(id);
+            if (line == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(line);
+        }
+
+        // PUT: api/Lines/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutLine(int id, Line line)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != line.Id)
+            {
+                return BadRequest();
+            }
+
+            db.Lines.Update(line);
+
+            try
+            {
+                db.Complete();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!LineExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+        
+        // POST: api/Lines
+        [ResponseType(typeof(Line))]
+        public IHttpActionResult PostLine(Line line)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Lines.Add(line);
+            db.Complete();
+
+            return CreatedAtRoute("DefaultApi", new { id = line.Id }, line);
+        }
+
+        // DELETE: api/Lines/5
+        [ResponseType(typeof(Line))]
+        public IHttpActionResult DeleteLine(int id)
+        {
+            Line line = db.Lines.Get(id);
+            if (line == null)
+            {
+                return NotFound();
+            }
+
+            db.Lines.Remove(line);
+            db.Complete();
+
+            return Ok(line);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool LineExists(int id)
+        {
+            return db.Lines.GetAll().Count(e => e.Id == id) > 0;
+        }
+    }
+}
